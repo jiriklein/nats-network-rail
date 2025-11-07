872d38845c3f4fbedf0754fd53c8137392f2a868
Jiri Klein <jiri.klein@bezerocarbon.com>
2025-11-07 18:33:09 +0000

    branch init and detailing the work in TODO.md

--- connect_and_subscribe.py
+++ connect_and_subscribe.py
@@ -121,11 +121,9 @@ class NRClient(stomp.ConnectionListener):
                 loc_stanox = str(
                     body.get("loc_stanox") or body.get("location_stanox") or ""
                 )
-                if loc_stanox in STATIONS_ALLOWLIST:
-                    logger.info("Message: %s", str(body)[:50])
-                    self.bridge.publish_to_nats(
-                        subject=NATS_SUBJECT, data=json.dumps(body)
-                    )
+                self.bridge.publish_to_nats(
+                    subject=NATS_SUBJECT, data=json.dumps(body)
+                )
 
         except Exception as exc:
             logger.error(exc)
